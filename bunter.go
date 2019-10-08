2d10f0b596161d663c88cf8e81bd823eda0a19e7
Jose Vasconcellos <jose@metologica.com>
2019-10-08 06:31:50 -0500

    Add support for additional indexes (not working)

--- bunter.go
+++ bunter.go
@@ -14,7 +14,7 @@ func openBunt(dbName *string) *buntdb.DB {
 	// Open the data.db file. It will be created if it doesn't exist.
 	db, err := buntdb.Open(*dbName)
 	if err != nil {
-		fmt.Print(err)
+		fmt.Println(err)
 		os.Exit(1)
 	}
 	return db
@@ -25,24 +25,41 @@ func main() {
 	var value string
 	var db *buntdb.DB
 
+	app := kingpin.New("bunter", "command line interface to buntdb")
 	// kingpin.Flag("bunter", "buntdb tool")
-	kingpin.Version(revision)
-	dbName := kingpin.Flag("db", "File name").String()
+	app.DefaultEnvars()
+	app.Version(revision)
+	dbName := app.Flag("db", "File name").String()
 
-	cmdGet := kingpin.Command("get", "get value")
+	cmdGet := app.Command("get", "get value")
 	cmdGet.Arg("key", "key").Required().StringVar(&key)
 
-	cmdSet := kingpin.Command("set", "Set key value")
+	cmdSet := app.Command("set", "Set key value")
 	cmdSet.Arg("key", "key").Required().StringVar(&key)
 	cmdSet.Arg("value", "value").Required().StringVar(&value)
 
-	cmdFind := kingpin.Command("find", "find")
+	cmdFind := app.Command("find", "find")
 	cmdFind.Arg("key", "key").Required().StringVar(&key)
 
-	switch kingpin.Parse() {
+	cmdIndex := app.Command("index", "create index")
+	cmdIndex.Arg("index", "index name").Required().StringVar(&key)
+	cmdIndex.Arg("pattern", "pattern").Required().StringVar(&value)
+
+	app.Command("indexes", "list indexes")
+
+	cmdDelIndex := app.Command("delindex", "delete key")
+	cmdDelIndex.Arg("index", "name").Required().StringVar(&key)
+
+	cmdDel := app.Command("del", "delete key")
+	cmdDel.Arg("key", "key").Required().StringVar(&key)
+
+	cmd, err := app.Parse(os.Args[1:])
+	kingpin.FatalIfError(err, "Argument error")
+	db = openBunt(dbName)
+	defer db.Close()
+
+	switch cmd {
 	case "get":
-		db = openBunt(dbName)
-		defer db.Close()
 		db.View(func(tx *buntdb.Tx) error {
 			val, err := tx.Get(key)
 			if err != nil {
@@ -54,8 +71,6 @@ func main() {
 		})
 
 	case "set":
-		db = openBunt(dbName)
-		defer db.Close()
 		db.Update(func(tx *buntdb.Tx) error {
 			_, _, err := tx.Set(key, value, nil)
 			if err != nil {
@@ -65,15 +80,55 @@ func main() {
 			return err
 		})
 
-	case "find":
-		db = openBunt(dbName)
-		defer db.Close()
+	case "del":
+		db.Update(func(tx *buntdb.Tx) error {
+			_, err := tx.Delete(key)
+			if err != nil {
+				fmt.Printf("error - %v\n", err)
+				return err
+			}
+			return err
+		})
 
+	case "find":
 		db.View(func(tx *buntdb.Tx) error {
 			tx.AscendKeys(key, func(k, v string) bool {
 				fmt.Printf("%s: %v\n", k, v)
 				return true
 			})
+			return nil
+		})
+
+	case "index":
+		db.Update(func(tx *buntdb.Tx) error {
+			err := tx.CreateIndex(key, value, buntdb.IndexString)
+			if err != nil {
+				fmt.Printf("error - %v\n", err)
+			}
+			return err
+		})
+
+	case "delindex":
+		db.Update(func(tx *buntdb.Tx) error {
+			err := tx.DropIndex(key)
+			if err != nil {
+				fmt.Printf("drop index - %v\n", err)
+			}
+			return err
+		})
+
+	case "indexes":
+		db.View(func(tx *buntdb.Tx) error {
+			list, err := tx.Indexes()
+			if err != nil {
+				fmt.Printf("error - %v\n", err)
+				return err
+			}
+			if len(list) == 0 {
+				fmt.Print("No additional indexes are defined\n")
+			} else {
+				fmt.Printf("%v\n", list)
+			}
 			return nil
 		})
 	}
